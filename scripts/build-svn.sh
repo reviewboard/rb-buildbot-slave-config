887290b25a25dd01a1f390e530e3da1c62574b39
chipx86 <chipx86@5efc13c4-1f27-0410-8691-ff2d1f55687e>
2009-07-09 09:03:44 +0000

    Always run backport when building pysvn. Now we can use one version for every version of Python.
    git-svn-id: https://reviewboard.googlecode.com/svn/trunk/buildbot/conf/slave@2035 5efc13c4-1f27-0410-8691-ff2d1f55687e

--- scripts/build-svn.sh
+++ scripts/build-svn.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
+PACKAGE_NAME="pysvn-1.6.3"
+FILENAME="$PACKAGE_NAME.tar.gz"
+DOWNLOAD_URL="http://pysvn.barrys-emacs.org/source_kits/$FILENAME"
+
 for dirname in lib/python*; do
 	PYTHON=`basename $dirname`
 	SITE_PACKAGES="lib/$PYTHON/site-packages"
-
-	if test "$PYTHON" = "python2.6"; then
-		PACKAGE_NAME="pysvn-1.6.3"
-	else
-		PACKAGE_NAME="pysvn-1.6.2"
-	fi
-
-	FILENAME="$PACKAGE_NAME.tar.gz"
-	DOWNLOAD_URL="http://pysvn.barrys-emacs.org/source_kits/$FILENAME"
 
 	if test ! -e "$FILENAME" -o ! -e $SITE_PACKAGES/pysvn; then
 		wget $DOWNLOAD_URL -O $FILENAME
 		tar -xzvf $FILENAME
 		cd $PACKAGE_NAME/Source
+		$PYTHON setup.py backport
 		$PYTHON setup.py configure || exit 1
 		make || exit 1
 		mv pysvn ../../$SITE_PACKAGES || exit 1
